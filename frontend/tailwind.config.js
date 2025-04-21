bde6a8978ce7ec77b9afaf6e8c33da4e04d7a955
visheshpanchal27 <visheshpanchal1212.com>
2025-04-21 19:37:03 +0530

    Fix vite and tailwind config for Vercel deployment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-import flowbite from 'flowbite/plugin'
-
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [flowbite],
+  plugins: [require('flowbite/plugin')],
 };
