55e55e18afeb5d7ce7eb8d601b94ad11cee1b6dc
visheshpanchal27 <visheshpanchal1212.com>
2025-04-22 12:33:20 +0530

    First commit: frontend and backend setup

--- frontend/src/pages/Products/ProductCard.jsx
+++ frontend/src/pages/Products/ProductCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../../redux/features/CartTemp/CartSlice";
+import { addToCart } from "../../redux/features/Cart/CartSlice";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import HeartIcon from "./HeartIcon";
