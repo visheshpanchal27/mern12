7456a8a977641ebb4aeb707c183551883f238684
Vishesh Panchal <visheshpanchal1212@gmail.com>
2025-04-22 09:48:02 +0530

    Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import Loader from "../components/Loader";
 import Header from "../components/Header";
 import Massage from "../components/Massage";
-import Product from "./Products/ProductAll.jsx";
+import ProductAll from "./Products/ProductAll.jsx";
 import { useState, useEffect } from "react";
 
 const Home = () => {
