4865b84dbcfbeb58baaf500798d15720b79491c0
Vishesh Panchal <visheshpanchal1212@gmail.com>
2025-04-25 12:24:00 +0530

    Update apiSlice.js

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -3,7 +3,6 @@ import { BASE_URL } from '../constants';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
-  credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.userInfo?.token;
     if (token) {
