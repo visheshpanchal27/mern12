8237b0fd74554ba4d77658519a4507d825c2865f
Vishesh Panchal <visheshpanchal1212@gmail.com>
2025-04-25 12:42:14 +0530

    Update apiSlice.js

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -2,19 +2,21 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://mern12-y4o1.onrender.com/api',
-    credentials: 'include', 
-    prepareHeaders: (headers, { getState }) => {
-      const token = getState().auth.userInfo?.token;
-      console.log("🔐 TOKEN FROM REDUX:", token); 
+    baseQuery: fetchBaseQuery({
+  baseUrl: 'https://mern12-y4o1.onrender.com/api',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState()?.auth?.userInfo?.token;
+    console.log("TOKEN FROM REDUX:", token);
 
       if (token) {
         headers.set('Authorization', `Bearer ${token}`);
       }
+
       return headers;
-    },
-  }),
+      },
+    }),
+
   tagTypes: ['Product', 'Order', 'User'],
   endpoints: (builder) => ({}), 
 });
